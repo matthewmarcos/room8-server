a3c5e266240a8c30e8b9f93f0bad6826aa5c6b3d
Matthew Marcos <matthewmarcos94@gmail.com>
2016-12-21 19:50:10 +0800

    Router receive request from client

--- app.js
+++ app.js
@@ -5,42 +5,39 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./config/router');
+var api = require('./config/router');
 
 var app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
+// error handlers development error handler will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.sendStatus(err.status || 500);
-  });
+  app
+    .use(function (err, req, res, next) {
+      res.sendStatus(err.status || 500);
+    });
 }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.sendStatus(err.status || 500);
-});
-
+// production error handler no stacktraces leaked to user
+app
+  .use(function (err, req, res, next) {
+    res.sendStatus(err.status || 500);
+  });
 
 module.exports = app;
--- config/router.js
+++ config/router.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send('Working root!');
+    res.send({something: '!Working root!'});
 });
 
 module.exports = router;
