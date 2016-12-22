a0ba72607617730dc00da2daecd3ba59b7a99442
Matthew Marcos <matthewmarcos94@gmail.com>
2016-12-22 17:21:33 +0800

    Fix scaffold a little bit more

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var api = require('./config/router');
+const api = require('./config/router');
 
-var app = express();
+const app = express();
 
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -21,23 +21,21 @@ app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers development error handler will print stacktrace
 if (app.get('env') === 'development') {
-  app
-    .use(function (err, req, res, next) {
-      res.sendStatus(err.status || 500);
+    app.use(function (err, req, res, next) {
+        res.sendStatus(err.status || 500);
     });
 }
 
 // production error handler no stacktraces leaked to user
-app
-  .use(function (err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.sendStatus(err.status || 500);
-  });
+});
 
 module.exports = app;
