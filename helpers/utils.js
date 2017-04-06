91fd5469ab8b5b4dbcc988fdeef1ae3847061c39
Matthew Marcos <matthewmarcos94@gmail.com>
2017-04-06 09:18:21 +0800

    Commented utils in case it might be needed. Will deprecate in future commits

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,60 +1,60 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 
 
-const _checkType = (key, fieldType) => {
-    const type = typeof fieldType;
+// const _checkType = (key, fieldType) => {
+//     const type = typeof fieldType;
 
-    if(typeof key !== type) {
-        return false;
-    }
-    else {
-        return true;
-    }
-};
-
-
-const _getData = (src, fields) => {
-    const error = new Error('not found!');
-    let finalData = clone(fields),
-        checkArray = [],
-        isValid;
-
-    finalData = _.mapValues(finalData, (fieldType, key) => {
-        if(_checkType(src[key], fieldType)) {
-            return src[key];
-        }
-
-        return error;
-    });
-
-    checkArray = _.map(finalData, data => typeof data !== typeof error);
-
-    isValid = _.reduce(checkArray, (acc, x) => { return  x && acc }, true);
-
-    if(!isValid) {
-        return new Error('422');
-    }
-    else {
-        return finalData;
-    }
-};
+//     if(typeof key !== type) {
+//         return false;
+//     }
+//     else {
+//         return true;
+//     }
+// };
 
 
-export const clone = (obj) =>  {
-    return JSON.parse(JSON.stringify(obj));
-};
+// const _getData = (src, fields) => {
+//     const error = new Error('not found!');
+//     let finalData = clone(fields),
+//         checkArray = [],
+//         isValid;
+
+//     finalData = _.mapValues(finalData, (fieldType, key) => {
+//         if(_checkType(src[key], fieldType)) {
+//             return src[key];
+//         }
+
+//         return error;
+//     });
+
+//     checkArray = _.map(finalData, data => typeof data !== typeof error);
+
+//     isValid = _.reduce(checkArray, (acc, x) => { return  x && acc }, true);
+
+//     if(!isValid) {
+//         return new Error('422');
+//     }
+//     else {
+//         return finalData;
+//     }
+// };
 
 
-export const getData = (fields) => {
-    const from = (source) => {
-        return _getData(source, fields);
-    };
-
-    return { from };
-};
+// export const clone = (obj) =>  {
+//     return JSON.parse(JSON.stringify(obj));
+// };
 
 
-export const isEmail = (email) => {
-    return /^[a-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,3}$/.test(email.toLowerCase());
-};
+// export const getData = (fields) => {
+//     const from = (source) => {
+//         return _getData(source, fields);
+//     };
+
+//     return { from };
+// };
+
+
+// export const isEmail = (email) => {
+//     return /^[a-z0-9._%+-]+@[a-z0-9-]+\.[a-z]{2,3}$/.test(email.toLowerCase());
+// };
 
