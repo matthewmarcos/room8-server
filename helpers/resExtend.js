8036dab0997b8512d88f12dbd2e85ef19663cf28
Matthew Marcos <matthewmarcos94@gmail.com>
2017-04-09 20:36:44 +0800

    Pinaganda ang format ng ResExtend

--- helpers/resExtend.js
+++ helpers/resExtend.js
@@ -1,19 +1,15 @@
 import { toCamelCase } from 'case-converter'
 
-
 const ResponseExtender = () => {
     return (req, res, next) => {
         const oldSend = res.send;
-
         res.send = function(response) {
             // Convert everything res.send will send into camelcase
             res.send = oldSend;
             return res.send(toCamelCase(response));
         }
-
         next();
     };
-
 };
 
 export default ResponseExtender;
