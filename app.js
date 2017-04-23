36a5155b4b7b66863034fa886a475d603582f8eb
Matthew Marcos <matthewmarcos94@gmail.com>
2017-04-24 04:04:22 +0800

    Added apr24 dump

--- app.js
+++ app.js
@@ -73,6 +73,7 @@ app.use(function (req, res, next) {
 
 app.use(function (err, req, res, next) {
     if(!res.headersSent) {
+        console.error(err);
         return res.status(err.status || 500).send(err);;
     }
 });
@@ -80,6 +81,7 @@ app.use(function (err, req, res, next) {
 if (app.get('env') === 'development') {
     app.use(function (err, req, res, next) {
         if(!res.headersSent) {
+            console.error(err);
             return res.status(err.status || 500).send(err);;
         }
     });
