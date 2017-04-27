955185734cdf9c3751d8872b9fd26445b16b5827
Matthew Marcos <matthewmarcos94@gmail.com>
2017-04-28 05:55:44 +0800

    Inserted user2

--- misc/users/user2.sh
+++ misc/users/user2.sh
@@ -1,9 +1,9 @@
 #### Account Creation ####
 
-printf "Starting to create usera"
+printf "Starting to create user2"
     curl \
     --request POST \
-    --data "username=usera&password=password&email=usera%40email.com&nickname=Matthew" \
+    --data "username=user2&password=password&email=user2%40email.com&nickname=Matthew" \
     http://localhost:5001/auth/register
 
 sleep 1
@@ -13,88 +13,88 @@ printf "Logging In!\n"
     curl \
     --request POST \
     --cookie-jar ./cookies \
-    --data "username=usera&password=password" \
+    --data "username=user2&password=password" \
     http://localhost:5001/auth/login
 printf "\nFinished Loggin In!\n"
 
 sleep 2
 #### UPDATING USER ####
-printf "\bEditing usera\n"
+printf "\bEditing user2\n"
 #### Editing User Profile ####
-printf "\nusera: Shookt! Editing Profile\n";
+printf "\nuser2: Shookt! Editing Profile\n";
     curl \
     --request PUT \
     --cookie ./cookies \
-    --data "fullName=Matthew%20Macos&status=I%20am%20looking%20for%20a%20room&cleanliness=5&sex=Male&smoker=No&hasOrg=No&gender=Yugoslavian%20Apache%20Helicopter&course=BS%20Computer%20Science&batch=2017&birthday=%7B%25%20now%20'ISO-8601'%20%25%7D&contactNumber=09123456789&bio=Friedrich%20Wilhelm%20Nietzsche%20was%20a%20German%20philosopher,%20cultural%20critic,%20poet,%20philologist,%20and%20Latin%20and%20Greek%20scholar%20whose%20work%20has%20exerted%20a%20profound%20influence%20on%20Western%20philosophy%20and%20modern%20intellectual%20history.asdasd&nickname=matthew&email=usera%40gmail.com" \
+    --data "fullName=Matthew%20Macos&status=I%20am%20looking%20for%20a%20room&cleanliness=3&sex=Female&smoker=Yes&hasOrg=Yes&gender=Iron%20Wolf&course=BS%20Computer%20Science&batch=2013&birthday='12-14-1996'&contactNumber=09123456789&bio=Bio%20here&nickname=matthew&email=user2%40gmail.com" \
     http://localhost:5001/api/profile
 sleep 0.5
 
 #### Editing User Preferences ####
-printf "\nusera: Shookt! Inserting Utilities\n";
+printf "\nuser2: Shookt! Inserting Utilities\n";
     curl \
     --request PUT \
     --cookie ./cookies \
-    --data "airconditioning=No&laundry=No&cooking=No&gasStove=No&electricStove=No&microwave=No&waterKettle=No&internet=No&torrent=No&speedRequirement=0" \
+    --data "airconditioning=Yes&laundry=Yes&cooking=Yes&gasStove=Yes&electricStove=Yes&microwave=Yes&waterKettle=Yes&internet=Yes&torrent=Yes&speedRequirement=3" \
     http://localhost:5001/api/preferences/utilities
 sleep 0.5
 
-printf "\nusera: Shookt! Inserting When\n";
+printf "\nuser2: Shookt! Inserting When\n";
     curl \
     --request PUT \
     --cookie ./cookies \
-    --data "duration=End%20of%20semester&startDate=2014-04-27" \
+    --data "duration=End%20of%20semester&startDate=2014-04-30" \
     http://localhost:5001/api/preferences/when
 sleep 0.5
 
-printf "\nusera: Shookt! Inserting Cost\n";
+printf "\nuser2: Shookt! Inserting Cost\n";
     curl \
     --request PUT \
     --cookie ./cookies \
-    --data "rentPriceRangeStart=1500&rentPriceRangeEnd=3500&shouldIncludeUtilities=No&utilitiesPriceRangeStart=0&utilitiesPriceRangeEnd=1500" \
+    --data "rentPriceRangeStart=2000&rentPriceRangeEnd=5000&shouldIncludeUtilities=No&utilitiesPriceRangeStart=500&utilitiesPriceRangeEnd=2000" \
     http://localhost:5001/api/preferences/cost
 sleep 0.5
 
-printf "\nusera: Shookt! Inserting Sex\n";
+printf "\nuser2: Shookt! Inserting Sex\n";
     curl \
     --request PUT \
     --cookie ./cookies \
-    --data "sex=Male" \
+    --data "sex=Female" \
     http://localhost:5001/api/preferences/sex
 sleep 0.5
 
-printf "\nusera: Shookt! Inserting Misc\n";
+printf "\nuser2: Shookt! Inserting Misc\n";
     curl \
     --request PUT \
     --cookie ./cookies \
-    --data "curfew=No&curfewTime=21:30:00&message=Some%20asdasdrandom%20message%20here%20makes%20these%20things%20awesome" \
+    --data "curfew=No&curfewTime=22:00:00&message=Some%20asdasdrandom%20message%20here%20makes%20these%20things%20awesome" \
     http://localhost:5001/api/preferences/misc
 sleep 0.5
 
 
-printf "\nusera: Shookt! Inserting Location\n";
+printf "\nuser2: Shookt! Inserting Location\n";
     curl \
     --request PUT \
     --cookie ./cookies \
-    --data "nearbyRestaurants=Yes&travelTimeToUplb=1&generalLocation=Demarces" \
+    --data "nearbyRestaurants=Do%20not%20care&travelTimeToUplb=10&generalLocation=Dema" \
     http://localhost:5001/api/preferences/location
 sleep 0.5
 
 
-printf "\nusera: Shookt! Inserting Lifestyle\n";
+printf "\nuser2: Shookt! Inserting Lifestyle\n";
     curl \
     --request PUT \
     --cookie ./cookies \
-    --data "alcohol=No&cleanliness=10&smokers=No&studyTime=Morning&guestsInRoom=Yes&guestsStudyArea=Yes&pets=Do%20not%20care&org=Do%20not%20care" \
+    --data "alcohol=Yes&cleanliness=10&smokers=Yes&studyTime=Evening&guestsInRoom=Yes&guestsStudyArea=No&pets=No" \
     http://localhost:5001/api/preferences/lifestyle
 sleep 0.5
 
-printf "\nusera: Fnished! Logging out!\n";
+printf "\nuser2: Fnished! Logging out!\n";
 
 #### Logout ####
     curl \
     --request POST \
     --cookie ./cookies \
     http://localhost:5001/auth/logout
-printf "\nusera: Fnished!\n\n";
+printf "\nuser2: Fnished!\n\n";
 sleep 0.5
 
